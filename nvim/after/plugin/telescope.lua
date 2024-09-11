cf80ba762dd8b7a97893222973bf2dbae979d98a
Felix Cuello <felix.cuello@gmail.com>
2024-09-11 16:31:06 -0300

    Changed telescope to horizontal

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -13,15 +13,16 @@ end
 
 require('telescope').setup {
   defaults = {
-    layout_strategy = "vertical",
+--    layout_strategy = "vertical",
+    layout_strategy = "horizontal",
     layout_config = {
-      vertical = {
-        width = 0.99,
-        height = 0.99,
-        preview_height = 0.6,
-        preview_cutoff = 0
-      },
-      horizontal = { width = 0.99 },
+--      vertical = {
+--        width = 0.99,
+--        height = 0.99,
+--        preview_height = 0.6,
+--        preview_cutoff = 0
+--      },
+      horizontal = { width = 0.99, height = 0.99 },
     },
   },
   pickers = {
