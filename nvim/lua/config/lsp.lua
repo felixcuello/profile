8437e02acd1b48b56b9721cd2b4cb1d65cf1f88e
Felix Cuello <felix.cuello@gmail.com>
2025-03-30 22:24:23 -0300

    feat(lsp): update LSP configuration for improved completion
    Refactored the LSP setup to use `vim.lsp.start` with a new `on_attach`
    function. Enabled autotrigger for completion and removed deprecated
    code. This improves the overall LSP integration and simplifies the
    configuration.

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -6,19 +6,16 @@ vim.lsp.enable({ 'ruby_lsp' })
 -- popup: Show extra information about the currently selected
 -- preview: Show extra information about the currently selected
 -- noinsert: Do not insert any text for a match until the user selects
+vim.lsp.start({
+  on_attach = function(client, bufnr)
+    vim.lsp.completion.enable(true, client.id, bufnr, {
+      autotrigger = true,
+--      convert = function(item)
+--        return { abbr = item.label:gsub('%b()', '') }
+--      end,
+    })
+  end,
+})
+
 vim.cmd("set completeopt+=menu,popup,preview,noinsert")
 vim.lsp.completion.enable()
-
-
--- vim.lsp.start({
---   name = 'ruby_lsp',
---   cmd = { 'BUNDLE_GEMFILE=Gemfile.34 ' + vim.fn.expand("~/.rbenv/shims/ruby-lsp") },
---   on_attach = function(client, bufnr)
---     vim.lsp.completion.enable(true, client.id, bufnr, {
---       autotrigger = true,
---       convert = function(item)
---         return { abbr = item.label:gsub('%b()', '') }
---       end,
---     })
---   end,
--- })
