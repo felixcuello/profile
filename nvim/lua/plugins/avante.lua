2b5cf4a6fed7bfbf3380625b88a1eb15648668e5
Felix Cuello <felix.cuello@gmail.com>
2025-04-13 12:29:25 -0300

    refactor(avante): enhance git commit message prompt
    - Improved the clarity of instructions in the commit message prompt
    - Updated formatting requirements to explicitly mention title length

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -6,7 +6,7 @@ return {
     { "<leader>aa", "<Cmd>AvanteAsk<Cr>", mode = { "n", "v" } },        -- Open Avante Ask
     { "<leader>af", "<Cmd>AvanteFocus<Cr>", mode = { "n", "v" } },      -- Focus Avante
     { "<leader>as", "<Cmd>AvanteStop<Cr>", mode = { "n", "v" } },       -- Stop Avante
-    { "<leader>cc", "<Cmd>AvanteAsk Please read the buffer and replace the git diff with the corresponding commit message for this change. The commit change must have a title 50 characters long, and a body that wraps at 72 characters with the proposed changes. The body should be formatted as a list of changes. The commit message should not include any author or co-author lines. Thanks!<Cr>", mode = { "n" } }, -- Generate commit message
+    { "<leader>cc", "<Cmd>AvanteAsk Please read the buffer and replace the git diff with the corresponding commit message for this change. The commit change must semantic commit message with a title 50 characters long, and a body that wraps at 72 characters with the proposed changes. The body should be formatted as a list of changes. The commit message should not include any author or co-author lines. Be careful to remove the first empty line at the beginning. Thanks!<Cr>", mode = { "n" } }, -- Generate commit message
   },
   opts = {
     provider = "copilot",
